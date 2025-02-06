ba94b2ffd511e27cef64e0905571a1259302668e
namcneal <namcneal@umich.edu>
2025-02-05 22:42:14 -0500

    sbatch still

--- cnn/cluster_run_all_kappa_cnn.sh
+++ cnn/cluster_run_all_kappa_cnn.sh
@@ -10,10 +10,11 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 
-RAW_DATA_DIR="/n/home02/nmcneal/jet_tagging/data"
-OUT_DATA_ROOT="/n/netscratch/reece_lab/Lab/noah/data_from_training_cnn"
+RAW_DATA_DIR=/n/home02/nmcneal/jet_tagging/data
+OUT_DATA_ROOT=/n/netscratch/reece_lab/Lab/noah/data_from_training_cnn
 
 module load python/3.10.13-fasrc01
 source activate pt2.3.0_cuda12.1
 
-python -c 'from generate_and_train_kappa import run_all_kappa as func; func($RAW_DATA_DIR, 10, 1000, $OUT_DATA_DIR)'
+COMMAND= $(python -c "from generate_and_train_kappa import run_all_kappa as f; print(f('$RAW_DATA_DIR', 10, 1000, '$OUT_DATA_ROOT'))")
+# python -c 'from generate_and_train_kappa import run_all_kappa as func; func($RAW_DATA_DIR, 10, 1000, $OUT_DATA_DIR)'
