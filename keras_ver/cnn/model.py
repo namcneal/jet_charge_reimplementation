c55f526b372d4e2d6c67de789aabb36bd532b41e
namcneal <namcneal@umich.edu>
2025-02-19 03:31:41 -0500

    Importing error was from a type...Now debugging until I can get the full results of the evaluation on one kappa.

--- keras_ver/cnn/model.py
+++ keras_ver/cnn/model.py
@@ -79,7 +79,7 @@ class CNNSpecification(object):
             final_dense_logits_activation='softmax',
 
             loss='categorical_crossentropy',
-            opt=Adam,
+            opt=keras.optimizers.Adam,
             lr=0.001,
             decay=0.0
         )
