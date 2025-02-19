5e7e0c9aff0399df2d3014480bef55728578d3fa
namcneal <namcneal@umich.edu>
2025-02-19 00:46:06 -0500

    Importing error was from a type...Now debugging until I can get the full results of the evaluation on one kappa.

--- keras_ver/cnn/main.py
+++ keras_ver/cnn/main.py
@@ -13,35 +13,6 @@ for directory in higher_directories:
 # at keras_ver/cnn/FilesystemNavigation.py
 from FileSystemNavigation import Directories, Filenames
 
-def main(args:argparse.Namespace):
-    directories = configure_system(args)
-    
-    from generate_images import JetChargeAttributes, generate_and_save_all_images
-    from data_loading    import MemmapDataset
-    from cnn import CNNSpecification, CNN
-
-    all_jet_data_seeds = range(args.min_data_seed, args.max_data_seed + 1)
-    all_kappas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-
-    for _, kappa in enumerate(all_kappas):
-        run_one_kappa(directories, kappa, all_jet_data_seeds)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--raw-jet-data-dir', type=str,   required=True)
-    parser.add_argument('--min-data-seed',  type=int,   required=True)
-    parser.add_argument('--max-data-seed',  type=int,   required=True)
-    parser.add_argument('--data-year', type=int,   required=True)
-    parser.add_argument('--energy-gev', type=int,   required=True)
-    parser.add_argument('--image-dir', type=str,   required=True)
-    parser.add_argument('--save-dir',  type=str,   required=True)
-    parser.add_argument('--batch_size', type=int,   default=512)
-    parser.add_argument('--num_epochs', type=int,   default=35)
-    parser.add_argument('--val_pct',    type=float, default=1/8)
-
-    main(parser.parse_args())
-
 def configure_system(args:argparse.Namespace):
     directories = Directories(
                     root_dir     = repository_root_directory,
@@ -72,4 +43,33 @@ def run_one_kappa(directories:Directories, kappa:float, jet_data_seeds:list[int]
     testing_labels = testing_dataset.labels
 
     evaluate_cnn(directories, filenames, testing_images_dataloader, testing_dataset.labels)
+
+def main(args:argparse.Namespace):
+    directories = configure_system(args)
+    
+    from generate_images import JetChargeAttributes, generate_and_save_all_images
+    from data_loading    import MemmapDataset
+    from cnn import CNNSpecification, CNN
+
+    all_jet_data_seeds = range(args.min_data_seed, args.max_data_seed + 1)
+    all_kappas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+
+    for _, kappa in enumerate(all_kappas):
+        run_one_kappa(directories, kappa, all_jet_data_seeds)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--raw-jet-data-dir', type=str,   required=True)
+    parser.add_argument('--min-data-seed',  type=int,   required=True)
+    parser.add_argument('--max-data-seed',  type=int,   required=True)
+    parser.add_argument('--data-year', type=int,   required=True)
+    parser.add_argument('--energy-gev', type=int,   required=True)
+    parser.add_argument('--image-dir', type=str,   required=True)
+    parser.add_argument('--save-dir',  type=str,   required=True)
+    parser.add_argument('--batch_size', type=int,   default=512)
+    parser.add_argument('--num_epochs', type=int,   default=35)
+    parser.add_argument('--val_pct',    type=float, default=1/8)
+
+    main(parser.parse_args())
 
