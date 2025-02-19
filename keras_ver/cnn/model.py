20425b723029906f8f0ba7316c8b19e72d32824e
namcneal <namcneal@umich.edu>
2025-02-19 02:28:18 -0500

    Importing error was from a type...Now debugging until I can get the full results of the evaluation on one kappa.

--- keras_ver/cnn/model.py
+++ keras_ver/cnn/model.py
@@ -8,7 +8,7 @@ for directory in higher_directories:
     if directory not in sys.path: sys.path.append(directory)
 
 ##
-import torch
+from torch.utils.data import DataLoader
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
