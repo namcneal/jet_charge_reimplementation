23936a35e7160153ce2fe47d94984a8866c77b35
namcneal <namcneal@umich.edu>
2025-02-24 16:22:37 -0500

    The folder organization is working alongside training and evaluation. I'm going to set the full program running as a batch job

--- keras_ver/cnn/cluster_main_training_evaluation_sbatch.sh
+++ keras_ver/cnn/cluster_main_training_evaluation_sbatch.sh
@@ -14,6 +14,7 @@ MIN_SEED=1
 MAX_SEED=2
 DATA_YEAR=2017
 DATA_ENERGY_GEV=1000
+NUM_EPOCHS=35
 
 RAW_DATA_DIR=/n/home02/nmcneal/jet_tagging/data
 OUT_JET_IMAGE_DIR=/n/netscratch/reece_lab/Lab/noah/data_from_training_cnn
@@ -23,7 +24,7 @@ module load python/3.10.13-fasrc01
 source activate pt2.3.0_cuda12.1
 
 PYTHON_SCRIPT=main.py
-python $PYTHON_SCRIPT --raw-jet-data-dir $RAW_DATA_DIR --min-data-seed $MIN_SEED --max-data-seed $MAX_SEED --data-year $DATA_YEAR --energy-gev $DATA_ENERGY_GEV --image-dir $OUT_JET_IMAGE_DIR --save-dir $SAVE_DATA_DIR
+python $PYTHON_SCRIPT --raw-jet-data-dir $RAW_DATA_DIR --min-data-seed $MIN_SEED --max-data-seed $MAX_SEED --data-year $DATA_YEAR --energy-gev $DATA_ENERGY_GEV --image-dir $OUT_JET_IMAGE_DIR --save-dir $SAVE_DATA_DIR --num-epochs $NUM_EPOCHS
 
 # For downloading from the cluster:
 # scp  nmcneal@login.rc.fas.harvard.edu:'/n/netscratch/reece_lab/Lab/noah/data_from_training_cnn/saved-data-for-1000GeV-*-tenth-kappa/roc_curve_for*.png'  S:\\home\\documents\\research\\jet_tagging\\results_from_cluster
